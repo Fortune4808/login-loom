ee055b1dc18bde337e0a5770154fbf67a1d324cc
FortuneTECH <opeyemiogunleye49@gmail.com>
2024-04-03 13:17:33 +0100

    Updated on 3rd of April.

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
 </head>
 
 <body>
+    
 <div class="overlay-div"></div>  
+
 <div class="loader-wrapper" id="loader-wrapper">
     <div id="loader"></div>
-        <div class="loader-section section-left"></div>
-        <div class="loader-section section-right"></div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
 </div>
 
     <?php include 'header.php';?>
